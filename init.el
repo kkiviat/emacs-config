737de1f673acf20a8d7961dfa0c5bdff48402617
Kira Kiviat <kkiviat@gmail.com>
2023-08-12 17:23:27 -0700

    Update some keybindings and change org-roam folder structure

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
                                     :repo "https://git.sr.ht/~bzg/org-contrib"
                                     :local-repo "org-contrib"))
 
-;; (straight-use-package '(org))
+(straight-use-package '(org))
 
 (org-babel-load-file "~/Dropbox/org/private.org")
 (org-babel-load-file (expand-file-name "settings.org" user-emacs-directory))
@@ -29,6 +29,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ignored-local-variable-values '((org-download-image-dir . "~/Dropbox/org/roam/images")))
  '(safe-local-variable-values '((org-download-image-dir . "~/Dropbox/org/roam/images"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
