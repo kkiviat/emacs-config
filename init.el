82d00bd64bc80bb7a98bca28e78707a37ff95373
Kira Kiviat <kkiviat@gmail.com>
2024-06-15 22:25:46 -0700

    Configure elfeed and elfeed-score

--- init.el
+++ init.el
@@ -27,6 +27,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(helm-minibuffer-history-key "M-p")
  '(safe-local-variable-values '((org-download-image-dir . "~/Dropbox/org/roam/images"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
